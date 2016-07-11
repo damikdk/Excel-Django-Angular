50f1ae4ed4609e17d9a4ce2b0b60339e501a93e2
damikdk <damikdk@gmail.com>
2016-07-11 19:38:36 +0300

    Trying...

--- web/models.py
+++ web/models.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
@@ -15,29 +17,31 @@ class OfficeFile(models.Model):
     def save(__self__):
         super(OfficeFile, __self__).save()
         xls = ExcelFile(__self__.file.path)
-        df = xls.parse(xls.sheet_names[0] )
+        df = xls.parse(xls.sheet_names[0])
         dict_data = df.to_dict('list')
 
+        # вычленяем из словаря список абсцисс
         if 'x' in dict_data:
             listx = dict_data['x']
         else:
             listx = dict_data['x_value']
-
+        # список ординат
         if 'y' in dict_data:
             listy = dict_data['y']
         else:
             listy = dict_data['y_value']
 
+        # превращаем numpyInt to Int
         i = 0
         while i < len(listx):
             listx[i] = int(listx[i])
-            i +=1
-
+            i += 1
         i = 0
         while i < len(listy):
             listy[i] = int(listy[i])
             i += 1
 
+        # Int обратно в словарь
         if 'x' in dict_data:
             dict_data['x'] = listx
         else:
