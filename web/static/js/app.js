ffa1e29dd7044f96e29c29da218287663f4225f3
damikdk <damikdk@gmail.com>
2016-07-11 18:28:29 +0300

    Deal with it

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,24 +19,53 @@ app.controller('OfficeFileController', function($scope, $http) {
     $scope.clickme = function (f) {
         var actualFile = JSON.parse(f.dict_coor);
         var array = [];
-        var minX = actualFile.x[0];
-        var maxX = actualFile.x[0];
+        console.log(actualFile);
 
-        for(var i=0; i<actualFile.x.length; i++) {
-            array.push([actualFile.x[i], actualFile.y[i]]);
+        for (key in actualFile) {
+            if (key == 'x_value') {
+                var minX = actualFile.x_value[0];
+                var maxX = actualFile.x_value[0];
 
-            if(actualFile.x[i] > maxX){
-                actualFile.x[i] = maxX;
-            } else if (actualFile.x[i] < minX) {
-                actualFile.x[i] = minX;
+                for(var i=0; i<actualFile.x_value.length; i++) {
+                    array.push([actualFile.x_value[i], actualFile.y_value[i]]);
+
+                    if(actualFile.x_value[i] > maxX){
+                        actualFile.x_value[i] = maxX;
+                    } else if (actualFile.x_value[i] < minX) {
+                        actualFile.x_value[i] = minX;
+                    }
+
+                    if(actualFile.y_value[i] > maxX){
+                        actualFile.y_value[i] = maxX;
+                    } else if (actualFile.y_value[i] < minX) {
+                        actualFile.y_value[i] = minX;
+                    }
+                }
             }
 
-            if(actualFile.y[i] > maxX){
-                actualFile.y[i] = maxX;
-            } else if (actualFile.y[i] < minX) {
-                actualFile.y[i] = minX;
+            if (key == 'x') {
+                var minX = actualFile.x[0];
+                var maxX = actualFile.x[0];
+
+                for(var i=0; i<actualFile.x.length; i++) {
+                    array.push([actualFile.x[i], actualFile.y[i]]);
+
+                    if(actualFile.x[i] > maxX){
+                        actualFile.x[i] = maxX;
+                    } else if (actualFile.x[i] < minX) {
+                        actualFile.x[i] = minX;
+                    }
+
+                    if(actualFile.y[i] > maxX){
+                        actualFile.y[i] = maxX;
+                    } else if (actualFile.y[i] < minX) {
+                        actualFile.y[i] = minX;
+                    }
+                }
             }
         }
+
+
 
         google.charts.setOnLoadCallback(drawChart);
         function drawChart() {
